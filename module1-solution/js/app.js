72c72d1e5c8cbb21f7af44db7ba57c23db03742a
aditya gathe <aditya gathe>
2016-10-02 20:04:10 +0530

    modulesolution

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -4,7 +4,21 @@ angular.module('Lunchcheck', [])
 .controller('LunchcheckController', LunchcheckController);
 function LunchcheckController ($scope) {
 
+$scope.displayArraylength= function ()
+{
+  if($scope.name==0)
+  {
+    $scope.length="";
+  };
+  var length =noOfItems($scope.name);
+  $scope.length=length;
+}
 
+function noOfItems(name)
+{
+  var items = name.length;
+  return items;
+}
  };
 
 
