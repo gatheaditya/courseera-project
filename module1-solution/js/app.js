0f554ae2aae63aed9eeedc45e7f6d78ee19abbf8
aditya gathe <aditya gathe>
2016-10-02 23:26:58 +0530

    modulesolution

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -3,34 +3,31 @@
 angular.module('Lunchcheck', [])
 .controller('LunchcheckController', LunchcheckController);
 
-function LunchcheckController ($scope) {
+function LunchcheckController ($scope, $injector) {
 
 $scope.displayArraylength= function ()
 {
  var length= checkIfEmpty($scope.name);
     $scope.length=length;
   }
-
 function checkIfEmpty(string)
 {
-var noofitems= string.split(",");
-if(noofitems.length>3)
-{
-  length="TOO MUCH";
-}
-else {
-length="ENJOY!"
-}
+  if(string!=null)
+  {
+    var noOfItems= string.split(",");
+    if(noOfItems.length>3)
+    {
+      length="TOO MUCH";
+    }
+    else {
+    length="ENJOY!"
+    }
+
+  }
+  else {
+length="PLEASE ENTER DATA";
+  }
 return length;
 }
-
-
  };
-
-
-
-
-
-
-
 })();
