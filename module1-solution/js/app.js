b65b5f5c5b7b94a80bc528b678493c6c626cbc54
aditya <gatheaditya@gmail.com>
2016-10-03 10:22:25 +0530

    moduel1final

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -3,13 +3,14 @@
 angular.module('Lunchcheck', [])
 .controller('LunchcheckController', LunchcheckController);
 
-function LunchcheckController ($scope, $injector) {
+function LunchcheckController ($scope,$injector) {
 
 $scope.displayArraylength= function ()
 {
   if($scope.name!=null)
   {
- var length= checkIfEmpty($scope.name);
+ var check = checkArraylength($scope.name);
+ var length= checkIfEmpty(check,$scope.name);
     $scope.length=length;
   }
   else
@@ -17,16 +18,40 @@ $scope.displayArraylength= function ()
      $scope.length="PLEASE ENTER DATA";
   }
   }
+  function checkArraylength(string)
+  {
+  var noOfItems= string.split(",");
+  var check=0;
+  var text = "";
+  var string= text.split(",");
+  var i;
+for (i = 0; i < noOfItems.length; i++) {
+if (noOfItems[i]=="") { break; }
 
-function checkIfEmpty(string)
+      text = i 
+}
+if(i==noOfItems.length)
 {
-  if(string==0|| string==undefined)
+text=1;
+}
+else
+{
+text=0;
+}
+return text;
+
+  }
+
+
+function checkIfEmpty(string,value)
+{
+  if(string==0)
   {
     
    length="PLEASE ENTER DATA";
   }
   else {
-    var noOfItems= string.split(",");
+    var noOfItems= value.split(",");
     if(noOfItems.length>3)
     {
       length="TOO MUCH";
