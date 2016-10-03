d6b7a097a170919552b47430d8c18d22b246cbde
aditya <gatheaditya@gmail.com>
2016-10-03 12:04:57 +0530

    module1

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -11,6 +11,7 @@ $scope.displayArraylength= function ()
   {
  var check = checkArraylength($scope.name);
  var length= checkIfEmpty(check,$scope.name);
+
     $scope.length=length;
   }
   else
@@ -27,9 +28,7 @@ $scope.displayArraylength= function ()
   var i;
 for (i = 0; i < noOfItems.length; i++) {
 if (noOfItems[i]=="") { break; }
-
-      text = i 
-}
+      text = i }
 if(i==noOfItems.length)
 {
 text=1;
@@ -39,9 +38,7 @@ else
 text=0;
 }
 return text;
-
   }
-
 
 function checkIfEmpty(string,value)
 {
@@ -63,5 +60,26 @@ function checkIfEmpty(string,value)
   }
 return length;
 }
+$scope.myObj =function styleElements(string)
+{
+  if(string==0)
+  $scope.myObj = {
+    "color" : "green",    
+    "padding" : "50px",
+     "border":"1px solid green"
+  }
+  else(string==1)
+  {
+    $scope.myObj = {
+    "color" : "red",    
+    "padding" : "50px",
+     "border":"1px solid green"
+  }
+  return $scope.myobj;
+  }
+}
+
+
+
  };
 })();
