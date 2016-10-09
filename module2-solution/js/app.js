9e953804cdce97b39d4424fe62341bc81b33c92e
gatheaditya <gatheaditya@gmail.com>
2016-10-10 00:54:41 +0530

    module2

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -2,25 +2,76 @@
   'use strict';
   angular.module("ShoppingList",[])
   .controller('ToBuyController', ToBuyController)
-  .controller('AlreadyBoughtController', AlreadyBoughtController );
-  ToBuyController.$inject=['$scope'];
- function ToBuyController($scope)
+  .controller('AlreadyBoughtController', AlreadyBoughtController )
+  .service('ShoppingListService',ShoppingListService);
+  ToBuyController.$inject=['ShoppingListService'];
+ function ToBuyController(ShoppingListService)
  {
-   var p = this;
-   p.name="test";
-    p.name2="pname2";
+   var toBuy = this;
+  toBuy.items= ShoppingListService.getToBuy();
+  toBuy.removeItem = function (itemIndex) {
+    ShoppingListService.removeItem(itemIndex);
+   };
+
+
+  console.log(toBuy.items);
+
  }
-   AlreadyBoughtController.$inject=['$scope'];
- function AlreadyBoughtController($scope)
+   AlreadyBoughtController.$inject=['ShoppingListService'];
+ function AlreadyBoughtController(ShoppingListService)
  {
-   var c = this;
-c.name="test2";
-c.name2="new cname2";
+   var bought = this;
+  bought.items  = ShoppingListService.getBought();
+bought.items=ShoppingListService.getBought();
 
  }
+function ShoppingListService()
+{
+  var service = this;
+  var toBuy ={
+    item:[{
+    name:'name1',
+    qty:3
+  },
+  {
+    name:'name2',
+    qty:3
+  }]
+};
+var alreadyBought =
+{
+  item:[{
+  name:'name1',
+  qty:3
+},
+{
+  name:'name2',
+  qty:3
+}]
 
+};
 
+service.additem= function (name,qty)
+{
+ var  item ={
+   name:name,
+   qty:qty
+ }
+ item.push(item);
+}
+service.removeItem = function (itemIndex) {
+  var  item ={
+    name:name,
+    qty:qty
+  }
+  item.push(item);
+   };
+service.getToBuy = function () {
+     return toBuy;
+   };
+  service.getBought = function () {
+        return alreadyBought;
+      };
 
-
-
+}
 })();
