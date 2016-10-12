2dd89f327c19373f8a2072ce9b5287a992959a54
gatheaditya <gatheaditya@gmail.com>
2016-10-12 21:04:35 +0530

    module2final

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -1,99 +1,30 @@
 (function(){
   'use strict';
-  angular.module("ShoppingList",[])
-  .controller('ToBuyController', ToBuyController)
-  .controller('AlreadyBoughtController', AlreadyBoughtController )
-  .service('ShoppingListService',ShoppingListService);
-  ToBuyController.$inject=['ShoppingListService'];
- function ToBuyController(ShoppingListService)
- {
-   var toBuy = this;
-   toBuy.isEmpty = function() {
-    return ShoppingListCheckOffService.boughtIsEmpty();
-  };
-  toBuy.items= ShoppingListService.getToBuy();
-  toBuy.shiftItems = function (itemIndex) {
-    ShoppingListService.shiftItems(itemIndex);
-   };
+  angular.module("NarrowItDownApp",[])
+  .controller('NarrowItDownController ', NarrowItDownController )
+  .service('MenuSearchService',MenuSearchService)
+  .directive('NarrowItDownControllerDirective',NarrowItDownControllerDirective);
 
+NarrowItDownController.$inject['MenuSearchService'];
+function NarrowItDownController(MenuSearchService)
+{
+var found=[];
+var list = this;
 
-  console.log(toBuy.items);
-
- }
-   AlreadyBoughtController.$inject=['ShoppingListService'];
- function AlreadyBoughtController(ShoppingListService)
- {
-   var bought = this;
-  bought.items  = ShoppingListService.getBought();
-  bought.isEmpty = function() {
-   return ShoppingListCheckOffService.boughtIsEmpty();
-  };
-
- }
-function ShoppingListService()
-{
-  var service = this;
-  var toBuy ={
-    item:[{
-    name:'cokkies',
-    qty:1
-  },
-  {
-  name:'cackes',
-  qty:2
-},
-{
-name:'ice-cream',
-qty:3
-},
-{
-name:'cokkies',
-qty:4
-},
-{
-name:'cokkies',
-qty:5
 }
-]
-};
-var alreadyBought =
-{
-  item:[]
 
-};
-
-service.additem= function (name,qty)
+function MenuSearchService()
 {
- var  item ={
-   name:name,
-   qty:qty
- }
- item.push(item);
+  var service= this;
+
+service.getMatchedMenuItems(searchItem)
+{
+
 }
-service.shiftItems = function (itemIndex) {
 
-  var name=toBuy.item[itemIndex].name;
-  var qty=toBuy.item[itemIndex].qty;
-  var  item ={
-    name:name,
-    qty:qty
-  }
-  alreadyBought.item.push(item);
-  toBuy.item.splice(itemIndex,1);
-   };
-service.getToBuy = function () {
-     return toBuy;
-   };
-  service.getBought = function () {
-        return alreadyBought;
-      };
-      service.boughtIsEmpty = function() {
-          return bought.item.length === 0;
-        };
+}
 
-        service.tobuyIsEmpty = function() {
-          return tobuy.item.length === 0;
-        };
+
 
 }
 })();
