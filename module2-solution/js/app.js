1f3dc2baea37bcdc71d0197ccfd547eb4be09359
gatheaditya <gatheaditya@gmail.com>
2016-10-12 22:31:10 +0530

    module2final

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -1,41 +1,102 @@
 (function(){
   'use strict';
-  angular.module("NarrowItDownApp",[])
-  .controller('NarrowItDownController ', NarrowItDownController )
-  .service('MenuSearchService',MenuSearchService)
-  .directive('NarrowItDownControllerDirective',NarrowItDownControllerDirective);
+  angular.module("ShoppingList",[])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController )
+  .service('ShoppingListService',ShoppingListService);
+  ToBuyController.$inject=['ShoppingListService'];
+ function ToBuyController(ShoppingListService)
+ {
+   var toBuy = this;
+   toBuy.isEmpty = function() {
+    return ShoppingListCheckOffService.boughtIsEmpty();
+  };
+  toBuy.items= ShoppingListService.getToBuy();
+  toBuy.shiftItems = function (itemIndex) {
+    ShoppingListService.shiftItems(itemIndex);
+   };
 
-NarrowItDownController.$inject['MenuSearchService'];
-function NarrowItDownController(MenuSearchService)
+
+  console.log(toBuy.items);
+
+ }
+   AlreadyBoughtController.$inject=['ShoppingListService'];
+ function AlreadyBoughtController(ShoppingListService)
+ {
+   var bought = this;
+  bought.items  = ShoppingListService.getBought();
+  bought.isEmpty = function() {
+   return ShoppingListCheckOffService.boughtIsEmpty();
+  };
+
+ }
+function ShoppingListService()
 {
-var found=[];
-var list = this;
-
-list.getItems = MenuSearchService.getMatchedMenuItems();
-console.log(list.getItems);
-
+  var service = this;
+  var toBuy ={
+    item:[{
+    name:'cokkies',
+    qty:1
+  },
+  {
+  name:'cackes',
+  qty:2
+},
+{
+name:'ice-cream',
+qty:3
+},
+{
+name:'cokkies',
+qty:4
+},
+{
+name:'cokkies',
+qty:5
 }
-
-MenuSearchService.$inject['$http']
-function MenuSearchService()
+]
+};
+var alreadyBought =
 {
-  var service= this;
+  item:[]
 
-service.getMatchedMenuItems()
+};
+
+service.additem= function (name,qty)
 {
-  var response = $http({
-      method:'GET',
-      url:'https://davids-restaurant.herokuapp.com/menu_items.json'
-    });
+ var  item ={
+   name:name,
+   qty:qty
+ }
+ item.push(item);
+}
+service.shiftItems = function (itemIndex) {
 
-    return response;
+  var name=toBuy.item[itemIndex].name;
+  var qty=toBuy.item[itemIndex].qty;
+  var  item ={
+    name:name,
+    qty:qty
   }
+  alreadyBought.item.push(item);
+  toBuy.item.splice(itemIndex,1);
+   };
+service.getToBuy = function () {
+     return toBuy;
+   };
+  service.getBought = function () {
+        return alreadyBought;
+      };
+      service.boughtIsEmpty = function() {
+          return bought.item.length === 0;
+        };
+
+        service.tobuyIsEmpty = function() {
+          return tobuy.item.length === 0;
+        };
 
 }
 
-}
 
 
-
-}
 })();
--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -1,99 +1,33 @@
 (function(){
   'use strict';
-  angular.module("ShoppingList",[])
-  .controller('ToBuyController', ToBuyController)
-  .controller('AlreadyBoughtController', AlreadyBoughtController )
-  .service('ShoppingListService',ShoppingListService);
-  ToBuyController.$inject=['ShoppingListService'];
- function ToBuyController(ShoppingListService)
- {
-   var toBuy = this;
-   toBuy.isEmpty = function() {
-    return ShoppingListCheckOffService.boughtIsEmpty();
-  };
-  toBuy.items= ShoppingListService.getToBuy();
-  toBuy.shiftItems = function (itemIndex) {
-    ShoppingListService.shiftItems(itemIndex);
-   };
+  angular.module("NarrowItDownApp",[])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService',MenuSearchService);
+  // .directive('NarrowItDownControllerDirective',NarrowItDownControllerDirective);
 
-
-  console.log(toBuy.items);
-
- }
-   AlreadyBoughtController.$inject=['ShoppingListService'];
- function AlreadyBoughtController(ShoppingListService)
- {
-   var bought = this;
-  bought.items  = ShoppingListService.getBought();
-  bought.isEmpty = function() {
-   return ShoppingListCheckOffService.boughtIsEmpty();
-  };
-
- }
-function ShoppingListService()
+NarrowItDownController.$inject=['MenuSearchService'];
+function NarrowItDownController(MenuSearchService)
 {
-  var service = this;
-  var toBuy ={
-    item:[{
-    name:'cokkies',
-    qty:1
-  },
-  {
-  name:'cackes',
-  qty:2
-},
-{
-name:'ice-cream',
-qty:3
-},
-{
-name:'cokkies',
-qty:4
-},
-{
-name:'cokkies',
-qty:5
+var found=[];
+var list = this;
+list.getItems = MenuSearchService.getMatchedMenuItems();
+console.log(list.getItems);
 }
-]
-};
-var alreadyBought =
+
+MenuSearchService.$inject=['$http'];
+function MenuSearchService($http)
 {
-  item:[]
-
-};
-
-service.additem= function (name,qty)
+  var service= this;
+service.getMatchedMenuItems= function()
 {
- var  item ={
-   name:name,
-   qty:qty
- }
- item.push(item);
-}
-service.shiftItems = function (itemIndex) {
+  var response = $http({
+      method:'GET',
+      url:'https://davids-restaurant.herokuapp.com/menu_items.json'
+    });
 
-  var name=toBuy.item[itemIndex].name;
-  var qty=toBuy.item[itemIndex].qty;
-  var  item ={
-    name:name,
-    qty:qty
+    return response;
   }
-  alreadyBought.item.push(item);
-  toBuy.item.splice(itemIndex,1);
-   };
-service.getToBuy = function () {
-     return toBuy;
-   };
-  service.getBought = function () {
-        return alreadyBought;
-      };
-      service.boughtIsEmpty = function() {
-          return bought.item.length === 0;
-        };
-
-        service.tobuyIsEmpty = function() {
-          return tobuy.item.length === 0;
-        };
 
 }
+
 })();
