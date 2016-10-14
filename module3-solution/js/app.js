674fdec2bacbae3ea533509c62dee426f60fa9d0
gatheaditya <gatheaditya@gmail.com>
2016-10-14 20:17:28 +0530

    odule3

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -8,25 +8,49 @@
 NarrowItDownController.$inject=['MenuSearchFactory','$http'];
 function NarrowItDownController(MenuSearchFactory,$http)
 {
-var found=[];
+
 var MenuSearchService =  MenuSearchFactory($http);
 var list = this;
+list.foundItem="";
+list.searchitem="";
 var promise = MenuSearchService.getMatchedMenuItems();
-     promise.then(function (response) {
-     list.getItems = response.data;
-     console.log(list.getItems.menu_items[0].id);
 
+list.itemFound = function(){
+
+   promise.then(function (response) {
+     list.getItems = response.data.menu_items;
+     list.getNoOfItems=response.data.menu_items.length;
+     list.foundItem=MenuSearchService.matchFoundItems(list.searchitem,list.getItems);
    })
    .catch(function (error) {
      console.log("Something went terribly wrong.");
    });
 
 }
-
+}
+//meanuservice
 MenuSearchFactory.$inject=['$http'];
 function MenuSearchService($http)
 {
   var service= this;
+  //function to get menu items
+
+service.matchFoundItems= function(searchitem,items)
+{
+ var found=[];
+  var i="";
+for (i = 0; i < items.length; i++) {
+    if (searchitem != items[i].name) { continue; }
+    found.push( items[i].id) ;
+    console.log(items[i].name);
+
+
+}
+
+  return found;
+}
+
+
 service.getMatchedMenuItems  = function()
 {
   var response = $http({
@@ -34,9 +58,18 @@ service.getMatchedMenuItems  = function()
       url:'https://davids-restaurant.herokuapp.com/menu_items.json'
     });
 
-    return response;
-  };
+    var data = response;
+    return data;
+
+  }
+
+
+
+
 };
+
+
+//factory to inject menuservice
 function MenuSearchFactory()
 {
 var factory = function($http){
