eb2b1dbaa15030d2f9d7372808d8441c2222ddd7
gatheaditya <gatheaditya@gmail.com>
2016-10-15 12:19:16 +0530

    module3

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -67,7 +67,7 @@ service.matchFoundItems= function(searchitem,items)
 if(searchitem.length>0)
 {
   var i="";
-
+found=[];
   for(i=0;i<items.length;i++)
 {
   var searchitem =searchitem.toLowerCase();
