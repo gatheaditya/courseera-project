ed7e91df0145dffdc32fc4f346c21ca4c36222b0
gatheaditya <gatheaditya@gmail.com>
2016-10-15 13:48:30 +0530

    module3

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -36,7 +36,7 @@ list.itemFound = function(){
      list.getItems = response.data.menu_items;
      list.getNoOfItems=response.data.menu_items.length;
      list.foundItem=MenuSearchService.matchFoundItems(list.searchitem,list.getItems);
-     list.isEmpty='';
+  
    })
    .catch(function (error) {
      console.log("Something went terribly wrong.");
