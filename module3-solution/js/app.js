c6a756353219bc1ebc220195eab6c80541f2ef6f
gatheaditya <gatheaditya@gmail.com>
2016-10-15 18:27:09 +0530

    module3

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -10,11 +10,12 @@
 function foundItems()
 {
   var ddo = {
-    templateUrl:'foundList.html',
+
 scope:{
   list: '<foundList'
 },
  controller:'isEmptyController as list',
+   templateUrl:'foundList.html',
  bindToController: true
 
   };
