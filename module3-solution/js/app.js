22d7d3cfcf08ea02d2866b677506c82efc1e6133
gatheaditya <gatheaditya@gmail.com>
2016-10-15 18:31:23 +0530

    module3

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -10,12 +10,12 @@
 function foundItems()
 {
   var ddo = {
-templateUrl:'foundList.html',
+
 scope:{
   list: '<foundList'
 },
- controller:'isEmptyController as list'
-
+ controller:'isEmptyController as list',
+ templateUrl:'foundList.html',
   };
   return ddo;
 }
