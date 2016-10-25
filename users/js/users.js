337fbcf6785f37f112fa4400b12c38fffb3f90ee
gatheaditya <gatheaditya@gmail.com>
2016-10-25 19:25:16 +0530

    module

---
+++ users/js/users.js
@@ -0,0 +1,22 @@
+(function(){
+
+  angular.module('users',[])
+  .controller('userController',userController)
+  .component('userComponent',userComponent)
+  .service('userService',userService);
+  function userComponent() {
+
+
+  }
+
+  function userController()
+  {
+
+
+
+  }
+  function userService() {
+
+  }
+
+})();
